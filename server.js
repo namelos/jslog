a054f5ac09ef2978eaa0ae174f355a92b9dfb21d
namelos <namelos@outlook.com>
2018-12-11 22:46:13 +0800

    creates and lists users

--- server.js
+++ server.js
@@ -8,4 +8,22 @@ server.get('/hello', (request, response) => {
   response.send('hello')
 })
 
+server.get('/users', (request, response) => {
+  response.json(db.users)
+})
+
+server.post('/users', (request, response) => {
+  const user = User(request.body)
+  db.users.push(user)
+  response.json(user)
+})
+
+const db = {
+  users: []
+}
+
+function User(params) {
+  return { username: params.username }
+}
+
 module.exports = server
--- server.test.js
+++ server.test.js
@@ -1,10 +1,33 @@
 const axiosist = require('axiosist')
 const server = require('./server')
 
-test('/hello returns hello', () => 
-  axiosist(server).get('/hello')
+test('/hello returns hello', () => {
+  return axiosist(server).get('/hello')
     .then(response => {
       expect(response.status).toBe(200)
       expect(response.data).toBe('hello')
     })
-)
+})
+
+test('GET /users lists user and POST /users creates user', () => {
+  return axiosist(server).get('/users')
+    .then(response => {
+      expect(response.status).toBe(200)
+      expect(response.data).toEqual([])
+      
+      return axiosist(server).post('/users', { username: 'John' })
+    })
+    .then(response => {
+      expect(response.status).toBe(200)
+      const user = response.data
+      expect(user.username).toBe('John')
+      
+      return axiosist(server).get('/users')
+    })
+    .then(response => {
+      expect(response.status).toBe(200)
+      expect(response.data.length).toBe(1)
+      const user = response.data[0]
+      expect(user.username).toBe('John')
+    })
+})

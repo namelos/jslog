c523aab4ae3a3a0a8643c5b4aaf94736f57f95aa
namelos <namelos@outlook.com>
2018-12-12 06:48:24 +0800

    split database module

---
+++ database.js
@@ -0,0 +1,31 @@
+const sqlite = require('sqlite')
+
+const dbPromise = sqlite.open(':memory:')
+  .then(db => {
+    return db.run(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(255) NOT NULL
+      )
+    `)
+    .then(() => db)
+  })
+  .catch(console.log)
+
+function all(...args) {
+  return dbPromise.then(db => db.all(...args))
+}
+
+function get(...args) {
+  return dbPromise.then(db => db.get(...args))
+}
+
+function run(...args) {
+    return dbPromise.then(db => db.run(...args))
+}
+
+module.exports = {
+  all,
+  get,
+  run
+}  
--- repositories.js
+++ repositories.js
@@ -1,34 +1,18 @@
-const sqlite = require('sqlite')
-
-const dbPromise = sqlite.open(':memory:')
-  .then(db => {
-    return db.run(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username VARCHAR(255) NOT NULL
-      )
-    `)
-    .then(() => db)
-  })
-  .catch(console.log)
+const database = require('./database')
 
 const userRepository = {
   all: function() {
-    return dbPromise
-      .then(db => db.all(`SELECT * FROM users`))
+    return database.all(`SELECT * FROM users`)
   },
   get: function(id) {
-    return dbPromise
-      .then(db => db.get(`SELECT * FROM users where id = ?`, id))
+    return database.get(`SELECT * FROM users where id = ?`, id)
   },
   insert: function(user) {
-    return dbPromise
-      .then(db => db.run(`INSERT INTO users (username) VALUES (?)`, user.username))
+    return database.run(`INSERT INTO users (username) VALUES (?)`, user.username)
       .then(result => userRepository.get(result.stmt.lastID))
   },
   clear: function() {
-    return dbPromise
-      .then(db => db.run(`DELETE FROM users`))
+    return database.run(`DELETE FROM users`)
   }
 }
 

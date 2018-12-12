a7a1cc2453ce87ed30651ffdc8435eb7046020d1
namelos <namelos@outlook.com>
2018-12-12 08:07:05 +0800

    add app component

--- public/client.js
+++ public/client.js
@@ -1,19 +1,49 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const axios = require('axios')
-const Hello = require('./Hello')
 
 const api = {
   getUsers: function() {
     return axios.get('/users')
+      .then(response => response.data)
   },
   createUser: function(username) {
     return axios.post('/users', { username: username })
-  },
-  createAndGetUsers: function(username) {
-    return api.createUser(username)
-      .then(() => api.getUsers())
+      .then(response => response.data)
   }
 }
 
-ReactDOM.render(<Hello />, document.querySelector('#root'))
+function App() {
+  const [users, setUsers] = React.useState([])
+  const [nameInput, setNameInput] = React.useState('')
+
+  React.useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  function fetchUsers() {
+    return api.getUsers()
+      .then(us => setUsers(us))
+  }
+
+  function handleCreateUserClick() {
+    if (nameInput) {
+      api.createUser(nameInput).then(fetchUsers)
+      setNameInput('')
+    }
+  }
+
+  return <div>
+    <ul>
+      {users.map((user, i) => <User {...user} key={i} />)}
+    </ul>
+    <input onChange={e => setNameInput(e.target.value)} value={nameInput}/>
+    <button onClick={handleCreateUserClick}>createUser</button>
+  </div>
+}
+
+function User({ username }) {
+  return <p>{username}</p>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))

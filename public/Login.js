53536ac7795188528c550856a032c11f2f82b83a
namelos <namelos@outlook.com>
2018-12-13 02:11:30 +0800

    let user login from client

--- public/Login.js
+++ public/Login.js
@@ -1,8 +1,22 @@
-import React from 'react'
+const React = require('react')
+const axios = require('axios')
 
 function Login() {
+  const [nameInput, setNameInput] = React.useState('')
+
+  async function handleClick() {
+    if (nameInput) {
+      const result = await axios.post('/session', { username: nameInput })
+      const session = result.data
+      const headers = { authorization: session.id }
+      await axios.get('/session', { headers })
+      setNameInput('')
+    }
+  }
+
   return <div>
-    login
+    username: <input value={nameInput} onChange={e => setNameInput(e.target.value)} />
+    <button onClick={handleClick}>Login</button>
   </div>
 }
 

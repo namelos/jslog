91a5e5be3482db6174c8a8917dc7b91f0a353eab
namelos <namelos@outlook.com>
2018-12-13 01:50:02 +0800

    change repository to async await syntax

--- repositories.js
+++ repositories.js
@@ -2,69 +2,58 @@ const uuid = require('uuid/v4')
 const database = require('./database')
 
 const userRepository = {
-  all: function() {
-    return database.all(`
-      SELECT *
-      FROM users
-    `)
+  async all() {
+    return await database.all(`SELECT * FROM users`)
   },
-  get: function(id) {
-    return database.get(`
-      SELECT *
-      FROM users
+  async get(id) {
+    return await database.get(`
+      SELECT * FROM users
       where id = ?
     `, id)
   },
-  getByUsername: function(username) {
-    return database.get(`
-      SELECT *
-      FROM users
+  async getByUsername(username) {
+    return await database.get(`
+      SELECT * FROM users
       where username = ?
     `, username)
   },
-  insert: function(user) {
-    return database.run(`
-      INSERT INTO
-        users (username)
+  async insert(user) {
+    const result = await database.run(`
+      INSERT INTO users
+        (username)
       VALUES (?)
     `, user.username)
-      .then(result => userRepository.get(result.stmt.lastID))
+    return await userRepository.get(result.stmt.lastID)
   },
-  clear: function() {
-    return database.run(`
-      DELETE FROM users
-    `)
+  async clear() {
+    return await database.run(`DELETE FROM users`)
   }
 }
 
 const sessionRepository = {
-  get: function(id) {
-    return database.get(`
-      SELECT *
-      FROM sessions
+  async get(id) {
+    return await database.get(`
+      SELECT * FROM sessions
       where id = ?
     `, id)
   },
-  insert: function(userId) {
+  async insert(userId) {
     const id = uuid()
-    return database.run(`
-      INSERT INTO
-        sessions (id, userId)
+    await database.run(`
+      INSERT INTO sessions
+        (id, userId)
       VALUES (?, ?)
     `, id, userId)
-      .then(() => sessionRepository.get(id))
+    return await sessionRepository.get(id)
   },
-  delete: function(userId) {
-    return database.run(`
+  async delete(userId) {
+    return await database.run(`
       DELETE FROM sessions
       where userId = ?
     `, userId)
   },
-  clear: function() {
-    return database.run(`
-      DELETE FROM sessions
-      where userId = ?
-    `)
+  async clear() {
+    return await database.run(`DELETE FROM sessions`)
   }
 }
 

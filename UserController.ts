4345aaec51b664fbcd7bc4ac179ebf265cb71c6c
namelos <namelos@outlook.com>
2019-01-16 23:57:56 +0800

    add user controller

---
+++ UserController.ts
@@ -0,0 +1,24 @@
+import { Get, JsonController, Post, Req } from 'routing-controllers'
+import { userRepository } from './repositories'
+
+@JsonController('/users')
+export class UserController {
+  @Get('/')
+  async getAll() {
+    return await userRepository.all()
+  }
+
+  @Post('/')
+  async create(@Req() request) {
+    const user = request.body
+    return await userRepository.insert(user)
+  }
+}
+
+class User {
+  username: string
+
+  constructor({ username }) {
+    this.username = username
+  }
+}
--- server.ts
+++ server.ts
@@ -1,15 +1,15 @@
+import 'reflect-metadata'
 import { createExpressServer } from 'routing-controllers'
 import { AuthenticationMiddleware } from './AuthenticationMiddleware'
 import { JsonMiddleware } from './JsonMiddleware '
 import { sessionApp } from './sessionApp'
 import { todoApp } from './todoApp'
-import { userApp } from './userApp'
+import { UserController } from './UserController'
 
 export const server = createExpressServer({
-  controllers: [],
+  controllers: [UserController],
   middlewares: [JsonMiddleware, AuthenticationMiddleware]
 })
 
-server.use('/users', userApp)
 server.use('/todos', todoApp)
 server.use('/session', sessionApp)

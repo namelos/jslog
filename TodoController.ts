f7e6e88fc2108d74fcf9cf5087a63a33f1449a33
namelos <namelos@outlook.com>
2019-01-17 00:08:26 +0800

    add todo controller

---
+++ TodoController.ts
@@ -0,0 +1,37 @@
+import { Get, JsonController, Post, Put, Req, Res } from 'routing-controllers'
+import { todoRepository } from './repositories'
+
+@JsonController('/todos')
+export class TodoController {
+  @Get('/')
+  async getAll(@Req() request) {
+    return await todoRepository.allByUser(request['session'].userId)
+  }
+
+  @Post('/')
+  async create(@Req() request) {
+    return await todoRepository.insert(new Todo({
+      content: request.body.content,
+      userId: request['session'].userId
+    }))
+  }
+
+  @Put('/:id')
+  async update(@Req() request, @Res() response) {
+    let todo = await todoRepository.get(request.params.id)
+    if (!todo) return response.status(404).send('Todo not found')
+    todo.content = request.body.content
+    todo.completed = request.body.completed
+    return await todoRepository.update(todo)
+  }
+}
+
+class Todo {
+  content: string
+  userId: number
+
+  constructor({ content, userId }) {
+    this.content = content
+    this.userId = userId
+  }
+}
--- server.ts
+++ server.ts
@@ -4,12 +4,12 @@ import { AuthenticationMiddleware } from './AuthenticationMiddleware'
 import { JsonMiddleware } from './JsonMiddleware '
 import { sessionApp } from './sessionApp'
 import { todoApp } from './todoApp'
+import { TodoController } from './TodoController'
 import { UserController } from './UserController'
 
 export const server = createExpressServer({
-  controllers: [UserController],
+  controllers: [UserController, TodoController],
   middlewares: [JsonMiddleware, AuthenticationMiddleware]
 })
 
-server.use('/todos', todoApp)
 server.use('/session', sessionApp)
